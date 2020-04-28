c5e402d3f20ec07f2fed665f14b1df2d2cbb8484
TraianC Endava <Traian.Ciobanu@endava.com>
2020-04-28 18:05:15 +0300

    small fix

--- examples/owasp-top-10/outputs.tf
+++ examples/owasp-top-10/outputs.tf
@@ -33,7 +33,7 @@ output "rule07_size_restriction_rule" {
   value       = "${module.owasp_top_10.rule07_size_restriction_rule_id}"
 }
 
-output "rule08_csrf_rule " {
+output "rule08_csrf_rule" {
   description = "AWS WAF Rule which enforces the presence of CSRF token in request header."
   value       = "${module.owasp_top_10.rule08_csrf_rule_id}"
 }
